934bbe202962a47ae4c7c580f4957d9f89b1b845
krunchyal <krunchyal@users.noreply.github.com>
2014-07-09 20:58:41 +0100

    Update assd.sh

--- assd.sh
+++ assd.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -u
 ##########################################################
 # Advanced Software Systems Development Module
 #
@@ -10,14 +10,6 @@
 # for UEA Linux
 ##########################################################
 
-###
-# Want to include some variables here
-# 
-# var1 = link to adtbundle
-# var2 = link to moduledocs.zip
-# var3 = path /opt/UEA/ASSD/
-###
-
 echo "Setting up Linux for Advanced Software Systems Development"
 echo "Updating package repositories..."
 sudo apt-get update -yqq
@@ -27,15 +19,15 @@ sudo apt-get update -yqq
 # Setup directories and permissions for ASSD module
 #
 ###########################################################
- 
+
 echo "Setting up directories..."
 
-sudo mkdir -p /opt/UEA/ASSD/
-sudo mkdir -p /home/uea_user/Desktop/ASSD/
-sudo chown -R nobody:users /opt/UEA/ASSD
-sudo chown -R uea_user:users /home/uea_user/Desktop/ASSD
-sudo chmod -R 777 /opt/UEA/ASSD 
-sudo chmod 777 -R /home/uea_user/Desktop/ASSD
+sudo mkdir -p "/opt/UEA/ASSD/"
+sudo mkdir -p "/home/uea_user/Desktop/ASSD/"
+sudo chown -R nobody:users "/opt/UEA/ASSD"
+sudo chown -R uea_user:users "/home/uea_user/Desktop/ASSD"
+sudo chmod -R 777 "/opt/UEA/ASSD"
+sudo chmod 777 -R "/home/uea_user/Desktop/ASSD"
 
 # Download module documentation (to be completed later)
 echo "Downloading module documentation (labsheets, source code, etc)..."
@@ -51,10 +43,10 @@ echo "Downloading module documentation (labsheets, source code, etc)..."
 ###########################################################
 
 echo "Installing pre-requisite packages for Eclipse..."
-sudo apt-get install -yqq curl expect default-jdk
+sudo apt-get install -y curl expect default-jdk git
 
 echo "Downloading Eclipse (ADT Bundle)..."
-cd /opt/UEA/ASSD
+cd "/opt/UEA/ASSD"
 wget http://dl.google.com/android/adt/adt-bundle-linux-x86_64-20140624.zip -O /opt/UEA/ASSD/adt-bundle.zip
 unzip /opt/UEA/ASSD/adt-bundle.zip
 rm adt-bundle.zip
@@ -63,17 +55,16 @@ ln -s /opt/UEA/ASSD/adt-bundle-linux-x86_64-20140624/eclipse/eclipse /home/uea_u
 ###########################################################
 #
 # Android SDK download and installation
-# 
+#
 # Uses expect to auto accept license agreement prompts
-# 
+#
 ###########################################################
- 
 
 echo "Downloading Android SDK components..."
 expect -c '
 set timeout -1   ;
-spawn sudo /opt/UEA/ASSD/adt-bundle-linux-x86_64-20140624/sdk/tools/android update sdk -u; 
-expect { 
+spawn sudo /opt/UEA/ASSD/adt-bundle-linux-x86_64-20140624/sdk/tools/android update sdk -a -u -t 1,2,3,16,17,27 ;
+expect {
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
 }
@@ -87,3 +78,4 @@ expect {
 
 echo "Complete."
 echo "Please use the new ASSD folder on your Desktop to launch Eclipse."
+
